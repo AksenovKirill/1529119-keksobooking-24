be5c26b870e8637401ee0c65ecb355949774302c
AksenovKirill <aksenovkirill@yandex.ru>
2021-10-16 11:25:14 +0300

    Отрисуй меня полностью часть 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 import {createOffer} from './data.js';
 import {createOfferElement} from './markup.js';
+import { getInactivateForm, getActivateForm } from './form.js';
+
+getInactivateForm();
 
 const OFFERS_AMOUNT = 10;
 const offers = Array.from({length: OFFERS_AMOUNT}, createOffer);
 
 // eslint-disable-next-line no-console
 console.log(offers);
+// eslint-disable-next-line no-console
+console.log(getActivateForm);
 
 offers.forEach(createOfferElement);
-
